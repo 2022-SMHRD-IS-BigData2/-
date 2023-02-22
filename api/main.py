fad0028dcaf5246501f5ac41863b477d9cf62ab3
kga9002 <kga9002@naver.com>
2023-02-22 14:26:15 +0900

    .

--- api/main.py
+++ api/main.py
@@ -14,7 +14,7 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 #static files & load
-app.mount("/", StaticFiles(directory="static", html = True), name="static") #<-- Add this
+app.mount("", StaticFiles(directory="static", html = True), name="static") #<-- Add this
 
 app.add_middleware(
   CORSMiddleware,
