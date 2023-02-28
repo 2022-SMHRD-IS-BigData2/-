9c2d2de468a48c012a06272c4cab73bdc6dc0419
intocage <j.b.woo739@gmail.com>
2023-02-28 09:32:11 +0900

    페이징(end가 지정안되고있어)

--- api/main.py
+++ api/main.py
@@ -91,14 +91,14 @@ async def get_view():
 
   return {"data": data}
 
-@app.get("/api/data")
+@app.get("/api/data/")
 async def get_data(limit: int = 10, page: int = 1):
     offset = (page - 1) * limit
     query = text(f"SELECT * FROM vital_record_now_view LIMIT :limit OFFSET :offset")
     result = session.execute(query, {"limit": limit, "offset": offset})
     data = [dict(row) for row in result]
     count = session.execute(text("SELECT COUNT(*) FROM vital_record_now_view")).fetchone()[0]
-    return {"data": data, "count": count}
+    return {"data": data, "count": count,'page':{'page':10,'limit':10}}
 
 def model_predict(record:VitalRecordAll):
   per=round(random.random()*100,2)
