70aa20f216c9d2145a61b4898df69c8d76f4fc0b
psh <kkxvz261@gmail.com>
2023-03-02 12:19:35 +0900

    .

--- static/js/app.js
+++ static/js/app.js
@@ -595,7 +595,7 @@ eval("// style-loader: Adds some css to the DOM by adding a <style> tag\n\n// lo
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "499c12997b8c4ba9"; }
+/******/ 		__webpack_require__.h = function() { return "5fe0c02fd534d86d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
